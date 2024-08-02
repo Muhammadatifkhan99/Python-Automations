ca81137e49246922769aeb59843257194d48a866
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-02 17:02:56 +0500

    reading and writing to a file project from QwikLab

---
+++ ReadingWriting.py
@@ -0,0 +1,15 @@
+guests = open("guests.txt", "w")
+initial_guests = ["Bob", "Andrea", "Manuel", "Polly", "Khalid"]
+
+for i in initial_guests:
+    guests.write(i + "\n")
+    
+guests.close()
+
+new_guests = ["Sam", "Danielle", "Jacob"]
+
+with open("guests.txt", "a") as guests:
+    for i in new_guests:
+        guests.write(i + "\n")
+
+guests.close()
--- removingfiles.py
+++ removingfiles.py
@@ -2,9 +2,14 @@
 #Removing file using code....
 
 import os
+#The OS module provides a layer of abstraction between the python and the operating system
 #os.remove("reomve.txt")
 
 
 # Renaming files using code...
 
-os.rename("atif.txt","atifkhan.txt")
+#os.rename("atifkhan.txt","atif.txt")
+
+
+os.path.exists("atif.txt")
+#os.path.exists("userlist.txt")
