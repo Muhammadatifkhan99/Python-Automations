f3c10c244bf827c78124e8ea582912a7b21fd8b9
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-02 16:29:23 +0500

    writing to files in python as well as reading from files

---
+++ writingtofile.py
@@ -0,0 +1,6 @@
+with open("novel.txt", "w") as file:
+    file.write("Hello! How are you")
+
+#different modes for opening the files
+# w is for writing, r is for read only by default is the r mode, a is for append which let to old content to stay safe.
+#r+ is for read and write
