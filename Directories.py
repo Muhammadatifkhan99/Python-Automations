28b2792adb416986cd63b6112485ab2d40eee5bb
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-06 07:43:06 +0500

    using pathlib and os.path libraries for creating,deleting and moving files around the directories

--- Directories.py
+++ Directories.py
@@ -13,10 +13,28 @@ import os
 # os.rmdir("atif")
 # os.rmdir("new dir")
 
-dir = "basicscripts"
+dir = "website"
 for name in os.listdir(dir):
      fullname = os.path.join(dir, name)
      if os.path.isdir(fullname):
           print("{} is a directory".format(fullname))
      else:
           print("{} is a file".format(fullname))
+
+# print(os.listdir("website"))
+# print(os.path.isdir("atif2"))
+
+
+################################################################################################################
+################################################################################################################
+################################################################################################################
+# Managing files and directories includes creating, deleting, and moving files and directories. It also includes 
+# changing ownership and permissions of
+# the files and directories. There are several ways to manage files and directories in Python. One of the easiest
+#  ways is to use low-level functions in
+# the OS and SYS modules that closely mimic standard Linux commands such as os.mkdir()and  os.rmdir().
+#  Alternatively, you can utilize the Pathlib 
+# module, which provides an object-oriented interface to working with the file systems. 
+################################################################################################################
+################################################################################################################
+################################################################################################################
