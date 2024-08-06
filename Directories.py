b19db45242c8c55e8cc8a67602bcc956a4187781
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-06 07:19:42 +0500

    finding files and folders inside a directory

--- Directories.py
+++ Directories.py
@@ -7,6 +7,16 @@ import os
 # os.chdir("new dir")
 # print(os.getcwd())
 
-# os.mkdir("atif")
+# os.mkdir("atif1")
+# os.mkdir("atif2")
+# os.mkdir("atif3")
 # os.rmdir("atif")
-os.rmdir("new dir")
+# os.rmdir("new dir")
+
+dir = "basicscripts"
+for name in os.listdir(dir):
+     fullname = os.path.join(dir, name)
+     if os.path.isdir(fullname):
+          print("{} is a directory".format(fullname))
+     else:
+          print("{} is a file".format(fullname))
