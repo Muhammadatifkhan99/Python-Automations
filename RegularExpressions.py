761c89c1dd72a8feabc2f68d9184962ba2587a37
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-10 00:21:21 +0500

    match different patterns in the regular expression and findall function to catch all the occurances of the of a word

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -1,23 +1,53 @@
 import re
 
 
-# result = re.search(r"aza","plaza")
-result = re.search(r"aza","bazaar")
-#the r inside the seach function indicates RAWSTRING and tells the 
-#interpreter to interpret the python string as it is............
+# # result = re.search(r"aza","plaza")
+# result = re.search(r"aza","bazaar")
+# #the r inside the seach function indicates RAWSTRING and tells the 
+# #interpreter to interpret the python string as it is............
+# print(result)
+
+# result = re.search(r"aza","Maze")
+# print(result)
+
+# result = re.search(r"^x","xenon")
+# print("Using circumflex: ",result)
+
+# result = re.search(r"p.ng","penguin")
+# print("Using the dot wildcard: ",result)
+
+
+# # to ignore a case sensitivity use the the function re.IGNORECASE
+
+# result = re.search(r"p.ng" ,"Pangea", re.IGNORECASE)
+# print(result)
+
+result = re.search(r"[Pp]ython", "Python")
+print(result) 
+
+
+result = re.search(r"[a-z]way"," This is the end of the highway")
+print("Specifying the range of the characters followed:",result)
+
+result = re.search(r"cloud[a-zA-Z0-9]","cloudy")
+print("Specifying the range of the characters:",result)
+
+result = re.search(r"cloud[a-zA-Z0-9]","cloud9")
+print("Specifying the range of the characters:",result)
+
+result = re.search(r"cloud[a-zA-Z0-9]","cloudY")
+print("Specifying the range of the characters:",result)
+
+result = re.search(r"cat|dog","dogs")
 print(result)
 
-result = re.search(r"aza","Maze")
-print(result)
-
-result = re.search(r"^x","xenon")
-print("Using circumflex: ",result)
-
-result = re.search(r"p.ng","penguin")
-print("Using the dot wildcard: ",result)
+# This function only defines the first occurance of the character matching the regax function 
 
 
-# to ignore a case sensitivity use the the function re.IGNORECASE
+result = re.search(r"cat|dog","I like both dogs cats")
+print("printing the first occurance of the match:",result)
 
-result = re.search(r"p.ng" ,"Pangea", re.IGNORECASE)
-print(result)
+# To solve this issue we use the findall function 
+
+result = re.findall(r"cat|dog","I like both dogs cats")
+print("printing the first occurance of the match:",result)
