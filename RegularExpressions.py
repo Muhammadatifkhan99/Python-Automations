aa76faf630474e1b955f924c075e864520437391
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-09 23:55:51 +0500

    Regular Expression Basics circumflex, dollar and dot wildcard characters

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -1,4 +1,23 @@
 import re
 
 
-result = re.search(r"aza","plaza")
+# result = re.search(r"aza","plaza")
+result = re.search(r"aza","bazaar")
+#the r inside the seach function indicates RAWSTRING and tells the 
+#interpreter to interpret the python string as it is............
+print(result)
+
+result = re.search(r"aza","Maze")
+print(result)
+
+result = re.search(r"^x","xenon")
+print("Using circumflex: ",result)
+
+result = re.search(r"p.ng","penguin")
+print("Using the dot wildcard: ",result)
+
+
+# to ignore a case sensitivity use the the function re.IGNORECASE
+
+result = re.search(r"p.ng" ,"Pangea", re.IGNORECASE)
+print(result)
