987eabe6fc4e18cf458d72157db2eccdb7e7db86
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2024-08-10 11:11:14 +0500

    Repeatition qualifiers and the escaping characters

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -22,32 +22,50 @@ import re
 # result = re.search(r"p.ng" ,"Pangea", re.IGNORECASE)
 # print(result)
 
-result = re.search(r"[Pp]ython", "Python")
-print(result) 
+# result = re.search(r"[Pp]ython", "Python")
+# print(result) 
 
 
-result = re.search(r"[a-z]way"," This is the end of the highway")
-print("Specifying the range of the characters followed:",result)
+# result = re.search(r"[a-z]way"," This is the end of the highway")
+# print("Specifying the range of the characters followed:",result)
 
-result = re.search(r"cloud[a-zA-Z0-9]","cloudy")
-print("Specifying the range of the characters:",result)
+# result = re.search(r"cloud[a-zA-Z0-9]","cloudy")
+# print("Specifying the range of the characters:",result)
 
-result = re.search(r"cloud[a-zA-Z0-9]","cloud9")
-print("Specifying the range of the characters:",result)
+# result = re.search(r"cloud[a-zA-Z0-9]","cloud9")
+# print("Specifying the range of the characters:",result)
 
-result = re.search(r"cloud[a-zA-Z0-9]","cloudY")
-print("Specifying the range of the characters:",result)
+# result = re.search(r"cloud[a-zA-Z0-9]","cloudY")
+# print("Specifying the range of the characters:",result)
 
-result = re.search(r"cat|dog","dogs")
-print(result)
+# result = re.search(r"cat|dog","dogs")
+# print(result)
 
 # This function only defines the first occurance of the character matching the regax function 
 
 
-result = re.search(r"cat|dog","I like both dogs cats")
-print("printing the first occurance of the match:",result)
+# result = re.search(r"cat|dog","I like both dogs cats")
+# print("printing the first occurance of the match:",result)
 
 # To solve this issue we use the findall function 
 
-result = re.findall(r"cat|dog","I like both dogs cats")
-print("printing the first occurance of the match:",result)
+# result = re.findall(r"cat|dog","I like both dogs cats")
+# print("printing the first occurance of the match:",result)
+
+
+# Repeatition Qualifiers:::
+
+# print(re.search(r"Py.*n", "Pygmalion"))
+# print(re.search(r"Py.*n", "Python Programming"))
+
+
+# print(re.search(r"Py[a-z]*n", "Python Programming"))
+
+# Escape characters::::::::::
+
+
+print(re.search(r".com","welcome"))
+
+print(re.search(r"\.com","www.google.com"))
+
+
